7974fd8bba334e99ada53657ec4c4fe32fdc183c
JesualdoPS <jesualdo_pinheiro@outlook.com>
2024-10-01 16:25:50 -0300

    Updated

---
+++ OnlineCourse.DomainTest/Courses/CourseStorageTest.cs
@@ -0,0 +1,63 @@
+﻿using Bogus;
+using Moq;
+using OnlineCourse.DomainTest._utilities;
+using OnlineCourse.DomainTest.Builders;
+
+namespace OnlineCourse.DomainTest.Courses
+{
+    public class CourseStorageTest
+    {
+        private readonly DtoCourse _dtoCourse;
+        private readonly CourseStorage _courseStorage;
+        private readonly Mock<ICourseRepository> _courseRepositoryMock;
+
+        public CourseStorageTest()
+        {
+            var fake = new Faker();
+
+            _dtoCourse = new DtoCourse
+            {
+                Name = fake.Random.Words(),
+                Workload = fake.Random.Double(10, 100),
+                TargetAudience = "Student",
+                Value = fake.Random.Double(100, 1000),
+                Description = fake.Lorem.Paragraph()
+            };
+
+            _courseRepositoryMock = new Mock<ICourseRepository>();
+
+            _courseStorage = new CourseStorage(_courseRepositoryMock.Object);
+        }
+        [Fact]
+        public void ShouldAddCourse()
+        {
+            _courseStorage.Store(_dtoCourse);
+
+            _courseRepositoryMock.Verify(r => r.Add(
+                It.Is<Course>(
+                    c => c.Name == _dtoCourse.Name &&
+                    c.Description == _dtoCourse.Description)));
+        }
+
+        [Fact]
+        public void ShouldNotAddInvalidTargetAudience()
+        {
+            var invalidTargetAudience = "Medic";
+            _dtoCourse.TargetAudience = invalidTargetAudience;
+
+            Assert.Throws<ArgumentException>(() => _courseStorage.Store(_dtoCourse))
+                .WithMessage("Invalid Target Audience");
+        }
+
+        [Fact]
+        public void ShouldNotAddCourseWithSameName()
+        {
+            var savedCourse = CourseBuilder.New().WithName(_dtoCourse.Name).Build();
+
+            _courseRepositoryMock.Setup(r => r.ChooseByName(_dtoCourse.Name)).Returns(savedCourse);
+
+            Assert.Throws<ArgumentException>(() => _courseStorage.Store(_dtoCourse))
+                .WithMessage("Name already exists in our database");
+        }
+    }
+}
---
+++ src/OnlineCourse.Domain/Courses/CourseStorage.cs
@@ -0,0 +1,41 @@
+﻿namespace OnlineCourse.DomainTest.Courses
+{
+    public class CourseStorage
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CourseStorage(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        public void Store(DtoCourse dtoCourse)
+        {
+            var savedCourse = _courseRepository.ChooseByName(dtoCourse.Name);
+
+            if (savedCourse != null)
+                throw new ArgumentException("Name already exists in our database");
+
+            if(!Enum.TryParse<TargetAudience>(dtoCourse.TargetAudience, out var targetAudience))
+                throw new ArgumentException("Invalid Target Audience");
+            
+            var course =
+                new Course(dtoCourse.Name, dtoCourse.Workload, targetAudience, dtoCourse.Value, dtoCourse.Description);
+
+            _courseRepository.Add(course);
+        }
+    }
+    public interface ICourseRepository
+    {
+        void Add(Course course);
+        Course ChooseByName(string course);
+    }
+    public class DtoCourse
+    {
+        public string Name { get; set; }
+        public double Workload { get; set; }
+        public string TargetAudience { get; set; }
+        public double Value { get; set; }
+        public string Description { get; set; }
+    }
+}
